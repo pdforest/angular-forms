b074c79d6de0412413b98a427594c83b9a1a3c9e
pdforest <pdforest@gmail.com>
2022-10-21 16:01:03 -0300

    Fin Seccion 17

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent implements OnInit{
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    terminos: [ false, Validators.requiredTrue]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: false
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      terminos: false
+    });
+
+    //this.miFormulario.valueChanges.subscribe( newForm => {
+    //  delete newForm.terminos;
+    //  this.persona = newForm;
+    //})
+    
+    this.miFormulario.valueChanges.subscribe( ({ terminos, ...restoDeLosArgumentos}) => {
+      this.persona = restoDeLosArgumentos;
+    })
+  }
+
+  guardar() {
+    if(this.miFormulario.invalid) {return;}
+
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.terminos;
+    this.persona = formValue;
+
+    console.log(this.persona);
+
   }
 
 }
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
-import { FormsModule } from '@angular/forms';
 import { CustomMinDirective } from './directives/custom-min.directive';
 
 
@@ -18,8 +18,8 @@ import { CustomMinDirective } from './directives/custom-min.directive';
   ],
   imports: [
     CommonModule,
-    TemplateRoutingModule,
-    FormsModule
+    FormsModule,
+    TemplateRoutingModule
   ]
 })
 export class TemplateModule { }
